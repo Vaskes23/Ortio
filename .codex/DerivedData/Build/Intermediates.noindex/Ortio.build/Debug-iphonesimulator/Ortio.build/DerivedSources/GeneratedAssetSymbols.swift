74165785e4ef4b4520998bbcef4db6cef6866c9f
matyasvascak <matyasvascak@gmail.com>
2026-06-10 10:00:52 +0200

    Add agent guidance for docs, capture, and tests

---
+++ .codex/DerivedData/Build/Intermediates.noindex/Ortio.build/Debug-iphonesimulator/Ortio.build/DerivedSources/GeneratedAssetSymbols.swift
@@ -0,0 +1,373 @@
+import Foundation
+#if canImport(AppKit)
+import AppKit
+#endif
+#if canImport(UIKit)
+import UIKit
+#endif
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+#if canImport(DeveloperToolsSupport)
+import DeveloperToolsSupport
+#endif
+
+#if SWIFT_PACKAGE
+private let resourceBundle = Foundation.Bundle.module
+#else
+private class ResourceBundleClass {}
+private let resourceBundle = Foundation.Bundle(for: ResourceBundleClass.self)
+#endif
+
+// MARK: - Color Symbols -
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension DeveloperToolsSupport.ColorResource {
+
+    /// The "AccentColor" asset catalog color resource.
+    static let accent = DeveloperToolsSupport.ColorResource(name: "AccentColor", bundle: resourceBundle)
+
+}
+
+// MARK: - Image Symbols -
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension DeveloperToolsSupport.ImageResource {
+
+    /// The "EnvironmentTips" asset catalog image resource.
+    static let environmentTips = DeveloperToolsSupport.ImageResource(name: "EnvironmentTips", bundle: resourceBundle)
+
+    /// The "ObjectCharacteristicsTips" asset catalog image resource.
+    static let objectCharacteristicsTips = DeveloperToolsSupport.ImageResource(name: "ObjectCharacteristicsTips", bundle: resourceBundle)
+
+    /// The "PhotographyTips" asset catalog image resource.
+    static let photographyTips = DeveloperToolsSupport.ImageResource(name: "PhotographyTips", bundle: resourceBundle)
+
+    /// The "ResetBbox" asset catalog image resource.
+    static let resetBbox = DeveloperToolsSupport.ImageResource(name: "ResetBbox", bundle: resourceBundle)
+
+    /// The "Reticle" asset catalog image resource.
+    static let reticle = DeveloperToolsSupport.ImageResource(name: "Reticle", bundle: resourceBundle)
+
+}
+
+// MARK: - Color Symbol Extensions -
+
+#if canImport(AppKit)
+@available(macOS 14.0, *)
+@available(macCatalyst, unavailable)
+extension AppKit.NSColor {
+
+    /// The "AccentColor" asset catalog color.
+    static var accent: AppKit.NSColor {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .accent)
+#else
+        .init()
+#endif
+    }
+
+}
+#endif
+
+#if canImport(UIKit)
+@available(iOS 17.0, tvOS 17.0, *)
+@available(watchOS, unavailable)
+extension UIKit.UIColor {
+
+    /// The "AccentColor" asset catalog color.
+    static var accent: UIKit.UIColor {
+#if !os(watchOS)
+        .init(resource: .accent)
+#else
+        .init()
+#endif
+    }
+
+}
+#endif
+
+#if canImport(SwiftUI)
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension SwiftUI.Color {
+
+    /// The "AccentColor" asset catalog color.
+    static var accent: SwiftUI.Color { .init(.accent) }
+
+}
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension SwiftUI.ShapeStyle where Self == SwiftUI.Color {
+
+    /// The "AccentColor" asset catalog color.
+    static var accent: SwiftUI.Color { .init(.accent) }
+
+}
+#endif
+
+// MARK: - Image Symbol Extensions -
+
+#if canImport(AppKit)
+@available(macOS 14.0, *)
+@available(macCatalyst, unavailable)
+extension AppKit.NSImage {
+
+    /// The "EnvironmentTips" asset catalog image.
+    static var environmentTips: AppKit.NSImage {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .environmentTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "ObjectCharacteristicsTips" asset catalog image.
+    static var objectCharacteristicsTips: AppKit.NSImage {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .objectCharacteristicsTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "PhotographyTips" asset catalog image.
+    static var photographyTips: AppKit.NSImage {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .photographyTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "ResetBbox" asset catalog image.
+    static var resetBbox: AppKit.NSImage {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .resetBbox)
+#else
+        .init()
+#endif
+    }
+
+    /// The "Reticle" asset catalog image.
+    static var reticle: AppKit.NSImage {
+#if !targetEnvironment(macCatalyst)
+        .init(resource: .reticle)
+#else
+        .init()
+#endif
+    }
+
+}
+#endif
+
+#if canImport(UIKit)
+@available(iOS 17.0, tvOS 17.0, *)
+@available(watchOS, unavailable)
+extension UIKit.UIImage {
+
+    /// The "EnvironmentTips" asset catalog image.
+    static var environmentTips: UIKit.UIImage {
+#if !os(watchOS)
+        .init(resource: .environmentTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "ObjectCharacteristicsTips" asset catalog image.
+    static var objectCharacteristicsTips: UIKit.UIImage {
+#if !os(watchOS)
+        .init(resource: .objectCharacteristicsTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "PhotographyTips" asset catalog image.
+    static var photographyTips: UIKit.UIImage {
+#if !os(watchOS)
+        .init(resource: .photographyTips)
+#else
+        .init()
+#endif
+    }
+
+    /// The "ResetBbox" asset catalog image.
+    static var resetBbox: UIKit.UIImage {
+#if !os(watchOS)
+        .init(resource: .resetBbox)
+#else
+        .init()
+#endif
+    }
+
+    /// The "Reticle" asset catalog image.
+    static var reticle: UIKit.UIImage {
+#if !os(watchOS)
+        .init(resource: .reticle)
+#else
+        .init()
+#endif
+    }
+
+}
+#endif
+
+// MARK: - Thinnable Asset Support -
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+@available(watchOS, unavailable)
+extension DeveloperToolsSupport.ColorResource {
+
+    private init?(thinnableName: Swift.String, bundle: Foundation.Bundle) {
+#if canImport(AppKit) && os(macOS)
+        if AppKit.NSColor(named: NSColor.Name(thinnableName), bundle: bundle) != nil {
+            self.init(name: thinnableName, bundle: bundle)
+        } else {
+            return nil
+        }
+#elseif canImport(UIKit) && !os(watchOS)
+        if UIKit.UIColor(named: thinnableName, in: bundle, compatibleWith: nil) != nil {
+            self.init(name: thinnableName, bundle: bundle)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+
+#if canImport(AppKit)
+@available(macOS 14.0, *)
+@available(macCatalyst, unavailable)
+extension AppKit.NSColor {
+
+    private convenience init?(thinnableResource: DeveloperToolsSupport.ColorResource?) {
+#if !targetEnvironment(macCatalyst)
+        if let resource = thinnableResource {
+            self.init(resource: resource)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+#endif
+
+#if canImport(UIKit)
+@available(iOS 17.0, tvOS 17.0, *)
+@available(watchOS, unavailable)
+extension UIKit.UIColor {
+
+    private convenience init?(thinnableResource: DeveloperToolsSupport.ColorResource?) {
+#if !os(watchOS)
+        if let resource = thinnableResource {
+            self.init(resource: resource)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+#endif
+
+#if canImport(SwiftUI)
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension SwiftUI.Color {
+
+    private init?(thinnableResource: DeveloperToolsSupport.ColorResource?) {
+        if let resource = thinnableResource {
+            self.init(resource)
+        } else {
+            return nil
+        }
+    }
+
+}
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension SwiftUI.ShapeStyle where Self == SwiftUI.Color {
+
+    private init?(thinnableResource: DeveloperToolsSupport.ColorResource?) {
+        if let resource = thinnableResource {
+            self.init(resource)
+        } else {
+            return nil
+        }
+    }
+
+}
+#endif
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+@available(watchOS, unavailable)
+extension DeveloperToolsSupport.ImageResource {
+
+    private init?(thinnableName: Swift.String, bundle: Foundation.Bundle) {
+#if canImport(AppKit) && os(macOS)
+        if bundle.image(forResource: NSImage.Name(thinnableName)) != nil {
+            self.init(name: thinnableName, bundle: bundle)
+        } else {
+            return nil
+        }
+#elseif canImport(UIKit) && !os(watchOS)
+        if UIKit.UIImage(named: thinnableName, in: bundle, compatibleWith: nil) != nil {
+            self.init(name: thinnableName, bundle: bundle)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+
+#if canImport(AppKit)
+@available(macOS 14.0, *)
+@available(macCatalyst, unavailable)
+extension AppKit.NSImage {
+
+    private convenience init?(thinnableResource: DeveloperToolsSupport.ImageResource?) {
+#if !targetEnvironment(macCatalyst)
+        if let resource = thinnableResource {
+            self.init(resource: resource)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+#endif
+
+#if canImport(UIKit)
+@available(iOS 17.0, tvOS 17.0, *)
+@available(watchOS, unavailable)
+extension UIKit.UIImage {
+
+    private convenience init?(thinnableResource: DeveloperToolsSupport.ImageResource?) {
+#if !os(watchOS)
+        if let resource = thinnableResource {
+            self.init(resource: resource)
+        } else {
+            return nil
+        }
+#else
+        return nil
+#endif
+    }
+
+}
+#endif
+
